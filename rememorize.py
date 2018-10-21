defce6a32d7f3a1f40f177524c497e13bb3b30d7
Lovac42 <lovac42@protonmail.com>
2018-10-20 23:47:50 -0400

    added changeEF and logging

--- rememorize.py
+++ rememorize.py
@@ -2,15 +2,24 @@
 # Copyright: (C) 2018 Lovac42
 # Support: https://github.com/lovac42/ReMemorize
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.4
+# Version: 0.0.5
 
 
-#config
-RANDOM_DAYS_MAX = 7
+# CONFIGS ##################################
 
+FUZZ_DAYS = True
+
+SIBLING_RESCHEDULE = True
 SIBLING_BOUNDARY = 90
+SIBLING_DAYS_MIN = 5    #Set equal days for no fuzz
+SIBLING_DAYS_MAX = 9    #Set equal days for no fuzz
 
-HOTKEY = 'Ctrl+M'
+HOTKEY = 'Ctrl+M' #SM hotkey?
+EF_HOTKEY = 'Ctrl+Shift+M'
+
+REVLOG_RESCHEDULED = True #Changes shows up in revlog
+
+# END CONFIGS ##################################
 
 
 from aqt import mw
@@ -18,8 +27,8 @@ from aqt.qt import *
 from anki.hooks import wrap
 from anki.sched import Scheduler
 from aqt.utils import showWarning, showText, getText
-from anki.utils import intTime
-import random
+from anki.utils import intTime, ids2str
+import random, time
 
 
 #from: anki.sched.Scheduler, removed resetting ease factor
@@ -27,16 +36,33 @@ def customReschedCards(ids, imin, imax):
     d = []
     t = mw.col.sched.today
     mod = intTime()
+    logtime = int(time.time()*1000)
     for id in ids:
         r = random.randint(imin, imax)
-        d.append(dict(id=id, due=r+t, ivl=max(1, r), mod=mod,
-                      usn=mw.col.usn()))
+        ivl = max(1, r)
+        d.append(dict(id=id, due=r+t, ivl=ivl, mod=mod, usn=mw.col.usn()))
+        card=mw.col.getCard(id)
+        try:
+            log(card,ivl,logtime)
+        except:
+            time.sleep(0.01) # duplicate pk; retry in 10ms
+            log(card,ivl,logtime)
+
     mw.col.sched.remFromDyn(ids)
     mw.col.db.executemany("""
 update cards set type=2,queue=2,ivl=:ivl,due=:due,odue=0,
 usn=:usn,mod=:mod where id=:id""", d)
     mw.col.log(ids)
 
+
+#custom log type: 4 = rescheduled
+#lastIvl = card.ivl
+def log(card, ivl, logtime):
+    if not REVLOG_RESCHEDULED: return
+    mw.col.db.execute(
+        "insert into revlog values (?,?,?,0,?,?,?,0,4)",
+        logtime, card.id, mw.col.usn(),
+        ivl, card.ivl, card.factor )
 
 
 class ReMemorize:
@@ -54,12 +80,19 @@ class ReMemorize:
         mnew.triggered.connect(self.forgetCards)
         menu.addAction(mnew)
 
-        mdays = QAction("re-memorize: Reschedule", mw)
+        mdays = QAction("re-memorize: Reschedule "+HOTKEY, mw)
         mdays.triggered.connect(self.ask)
         menu.addAction(mdays)
 
-        shortcut = QShortcut(QKeySequence(HOTKEY), mw) #supermemo hotkey?
+        cef = QAction("re-memorize: Change Card Factor "+EF_HOTKEY, mw)
+        cef.triggered.connect(self.changeEF)
+        menu.addAction(cef)
+
+        #Change due date
+        shortcut = QShortcut(QKeySequence(HOTKEY), mw)
         shortcut.activated.connect(self.ask)
+        shortcut = QShortcut(QKeySequence(EF_HOTKEY), mw)
+        shortcut.activated.connect(self.changeEF)
 
     def getSiblings(self, nid):
         return [i for i in mw.col.db.list(
@@ -74,7 +107,11 @@ class ReMemorize:
     def reschedCards(self, card, days):
         mw.col.markReview(card)
         cids=self.getSiblings(card.nid)
-        customReschedCards(cids, days, days+RANDOM_DAYS_MAX)
+        if FUZZ_DAYS:
+            min, max = mw.col.sched._fuzzIvlRange(days)
+            customReschedCards(cids, min, max)
+        else:
+            customReschedCards(cids, days, days)
         mw.reviewer._answeredIds.append(card.id)
         mw.autosave()
         mw.reset()
@@ -102,16 +139,25 @@ class ReMemorize:
             self.reschedCards(c, days)
 
 
+    def changeEF(self):
+        if mw.state != 'review': return
+        c=mw.reviewer.card
+        fct, ok = getText("Change Ease Factor:", default=str(c.factor))
+        if not ok: return
+        c.factor=max(1300,int(fct))
+        c.flush()
+
+
 remem=ReMemorize()
 
 
 #Reset sibling cards on forget
 def answerCard(self, card, ease):
-    if ease == 1:
+    if ease == 1 and SIBLING_RESCHEDULE:
         cids=[i for i in mw.col.db.list(
             "select id from cards where nid=? and type=2 and queue=2 and id!=? and ivl > ?",
             card.nid, card.id, SIBLING_BOUNDARY)]
         if len(cids) > 0:
-            customReschedCards(cids, 5, 9)
+            customReschedCards(cids, SIBLING_DAYS_MIN, SIBLING_DAYS_MAX)
 
 Scheduler.answerCard = wrap(Scheduler.answerCard, answerCard, 'after')
